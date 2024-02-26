4c04a9769cb7d13c8817d498e237f3cd7aba6129
Sanwal Khan <sanwal_khan_intern@netixsol.com>
2024-02-26 16:03:26 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const corsOptions: CorsOptions = {
-    origin: ['https://week-7-8-blogia-client.vercel.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  };
-
+  const app = await NestFactory.create(AppModule);
+  
   // Enable CORS with options
-  app.enableCors(corsOptions);
+  app.enableCors({
+    origin: 'https://week-7-8-blogia-client.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
